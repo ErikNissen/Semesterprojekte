afa238a371d4725d5b12d33e1bc799b9a6529023
nissene99 <43781812+nissene99@users.noreply.github.com>
2022-06-09 13:57:41 +0200

    init

---
+++ OOPProjekt/src/main/java/fhkiel/oopsemesterprojekt/enp/OOPProjekt/OopProjektApplication.java
@@ -0,0 +1,13 @@
+package fhkiel.oopsemesterprojekt.enp.OOPProjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopProjektApplication.class, args);
+	}
+
+}
---
+++ OOPProjekt/src/test/java/fhkiel/oopsemesterprojekt/enp/OOPProjekt/OopProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package fhkiel.oopsemesterprojekt.enp.OOPProjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
